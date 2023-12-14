6fc466fdd660bc8983e61806aca4e57da4dff693
Aung Hein <aungcho2003@gmail.com>
2023-12-14 16:23:56 +0630

    start

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require("express"),
+  app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.listen(3000, () => {
+  console.log("Server is running");
+});
